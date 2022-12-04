f28f7a6a0b600bbc2ef5241d2e8715af3f8c6071
Rafael Almeida <rafaelpbcp@gmail.com>
2022-12-04 16:05:55 -0500

    reformated code

--- b7-8-statistics/egg/simulation.py
+++ b7-8-statistics/egg/simulation.py
@@ -1,30 +1,31 @@
 from random import randint
-trials = 1000000
+games = 1000000
 
-p1lc = 0 # times player 1 lost
-p2lc = 0 
-for _ in range(trials):
-  # setting up a random carton
-  carton = [0]*12
-  while carton.count(1) < 4:
-    c = randint(0, 11)
-    carton[c] = 1 if carton[c] != 1 else 0
-  
-  p1s = 0 # player 1 score
-  p2s = 0 
-  while p1s < 2 and p2s < 2:
-    # player 1 choosing
-    p1c = randint(0, len(carton)-1)
-    p1s += 1 if carton[p1c] == 1 else 0
-    carton.pop(p1c)
+player1_losses = 0
+player2_losses = 0 
+for _ in range(games):
+  carton = ["boiled"]*12
+  while carton.count("raw") < 4:
+    i = randint(0, 11)
+    if carton[i] == "boiled":
+      carton[i] = "raw"
+
+  player1_raw_eggs = 0
+  player2_raw_eggs = 0
+  while player1_raw_eggs < 2 and player2_raw_eggs < 2:
+    player1_choice = randint(0, len(carton)-1)
+    if carton[player1_choice] == "raw":
+      player1_raw_eggs += 1
+    carton.pop(player1_choice)
     
-    p2c = randint(0, len(carton)-1)
-    p2s += 1 if carton[p2c] == 1 else 0
-    carton.pop(p2c)
+    player2_choice = randint(0, len(carton)-1)
+    if carton[player2_choice] == "raw":
+      player2_raw_eggs += 1
+    carton.pop(player2_choice)
 
-  if p1s == 2:
-    p1lc += 1
+  if player1_raw_eggs == 2:
+    player1_losses += 1
   else:
-    p2lc += 1
+    player2_losses += 1
   
-print(f"Player 1 lost {p1lc/trials} of the times while player 2 lost {p2lc/trials} of {trials} games.")
+print(f"Player 1 lost {player1_losses/games*100}% of the times while player 2 lost {player2_losses/games*100}% of {games} games.")
